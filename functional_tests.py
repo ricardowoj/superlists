900f28d7d78fb84c136f88a0b80b2d73d107d7b3
ricardowoj <ricardowoj@hotmail.com>
2020-07-07 22:55:36 -0300

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -15,21 +17,45 @@ class NewVisitorTest(unittest.TestCase):
         # lista de tarefas. Ela decide verificar sua homepage
         self.browser.get('http://localhost:8000')
 
-        # Ela percebe que o título da página e o cabeçalho mencionam list de tarefas (to-do)
-        self.assertIn('to-do', self.browser.title)
-        self.fail('Finish the test')
+        # Ela percebe que o título da página e o cabeçalho mencionam list de
+        # tarefas (To-Do lists)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Ela digit "Buy peacock feathers" em uma caixa
         # de texto (o hobby de Edith é fazer iscas para pesca com fly)
+        inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela teclar enter, a página é atualizada, e agora página lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
-        # que o site geru um URL único para ela -- há um pequeno
-        # text explicativo para isso.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar
+        # outro item. Ela insere  "Use peacock feathers to make a fly" (Usar
+        # penas de pavão para fazer um fly - Edith é metódica)
+        self.fail('Finish the test')
+
+        # A página é atualizada novamente e agora mostra os dois itens em sua
+        # lista
+
+        # Edith se pergunta se o site lebrará de sua lista. Então ela nota
+        # que o site gerou um URL único para ela -- há um pequeno
+        # texto explicativo para isso.
 
         # Ela acessa esse URL - sua lista de tarefas continua lá.
 
